444e907007d8347893ff443c3dda0a86fef2e7b2
Max <162749285+Cyber-Limon@users.noreply.github.com>
2024-11-20 12:59:27 +0500

    Update Calculation_methods_2.cpp

--- Calculation_methods_2.cpp
+++ Calculation_methods_2.cpp
@@ -1,30 +1,46 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 
 
 
 using namespace std;
 
-int main() {
-	setlocale(LC_ALL, "Russian");
+double func(int n, double x) {
+	if (n == 1)
+		return 6 * pow(x, 5);
+	else
+		return pow(x, 1 / 30) * pow(1 + pow(x, 2), 0.5);
+}
 
+
+
+double deriv(int n, double x) {
+	if (n == 1)
+		return 720 * x / 2880;
+	else
+		return 0;
+}
+
+
+
+void integral(int func_num, double pr, double a, double b, bool check) {
 	cout << "n\t" << "K_d\t\t" << "d_точное\t" << "d_Рунге\t\t" << "d_теор\n";
 	for (int i = 0; i < 70; i++) cout << "-";
 	cout << "\n";
 
-	
+
 
 	double J_old = 0;
 	double J_old_old = 0;
 
 	double K_d = 0;
-	int pr = 1;
+	double d_pr = 0;
 	double K_Run = 0;
 
 
 
 	for (int count_segments = 1; count_segments < 66000; count_segments *= 2) {
-		double h = 1.0 / count_segments;
+		double h = (b - a) / count_segments;
 
 
 
@@ -32,11 +48,11 @@ int main() {
 		double* array_y = new double[count_segments + 1];
 
 		for (int i = 0; i <= count_segments; i++) {
-			array_x[i] = h * i;
-			array_y[i] = 6 * pow(h * i, 5);
+			array_x[i] = h * i + a;
+			array_y[i] = func(func_num, array_x[i]);
 		}
 
-		
+
 
 		double sum_x_1 = 0;
 		double sum_x_2 = 0;
@@ -50,27 +66,29 @@ int main() {
 
 		double J = (h / 6) * (array_y[0] + array_y[count_segments] + 2 * sum_x_1 + 4 * sum_x_2);
 
-		
+
 
 		if (J_old_old != 0)
 			K_d = (J_old - J_old_old) / (J - J_old);
 
 
-
-		double d_pr = pr - J;
+		
+		if (check) 
+			d_pr = pr - J;
 
 
 
 		if (J_old != 0)
-			K_Run = (J- J_old) / (16 - 1);
-		
+			K_Run = (J - J_old) / (16 - 1);
+
 
 
 		double d_th = 0;
 
-		for (int i = 0; i <= count_segments; i++)
-			if (720 * array_x[i] / 2880 > d_th)
-				d_th = 720 * array_x[i] / 2880;
+		if (check) 
+			for (int i = 0; i <= count_segments; i++)
+				if (deriv(func_num, array_x[i]) > d_th)
+					d_th = deriv(func_num, array_x[i]);
 
 		d_th *= pow(h, 4);
 
@@ -81,21 +99,48 @@ int main() {
 
 
 
-		cout << "\t\t\t\t\t\t\t" << d_th << "\r";
+		cout << count_segments << "\r";
 
-		if (K_Run != 0) cout << "\t\t\t\t\t" << K_Run << "\r";
-		else cout << "\t\t\t\t\t" << "-\r";
+		if (K_d != 0) 
+			cout << "\t" << K_d << "\r";
+		else 
+			cout << "\t" << "-\r";
 
-		cout << "\t\t\t" << d_pr  << "\r";
+		if (check) 
+			cout << "\t\t\t" << d_pr << "\r";
+		else 
+			cout << "\t\t\t" << "-\r";
 
-		if (K_d != 0) cout << "\t" << K_d << "\r";
-		else cout << "\t" << "-\r";
+		if (K_Run != 0) 
+			cout << "\t\t\t\t\t" << K_Run << "\r";
+		else 
+			cout << "\t\t\t\t\t" << "-\r";
 
-		cout << count_segments << "\n";
+		if (check) 
+			cout << "\t\t\t\t\t\t\t" << d_th << "\n";
+		else 
+			cout << "\t\t\t\t\t\t\t" << "-\n";
 
-		
-		
+
+
 		delete[] array_x;
 		delete[] array_y;
 	}
+
+	cout << "\n\n\n";
+}
+
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Функция y = 6 * x^5 на отрезке [0, 1]\n\n";
+	integral(1, 1, 0, 1, true);
+
+	cout << "Функция y = x^(1/m) * (1+x^2)^(1/2) на отрезке [0, 1.5]\n\n";
+	integral(2, 0, 0, 1.5, false);
+
+	cout << "Функция y = x^(1/m) * (1+x^2)^(1/2) на отрезке [0.001, 1.5]\n\n";
+	integral(2, 0, 0.001, 1.5, false);
 }
